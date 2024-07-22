66f2fcb79fab575f5b3fc6f25443d033d69f5c6f
IvanDev <ivanjoelacosta605@gmail.com>
2024-07-22 11:44:50 -0400

    Consultar el Modelo en el Controlador y pasar Resultados hacia la vista

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -1,10 +1,14 @@
 <?php
     namespace Controllers;
     use MVC\Router;
+    use Model\Propiedad;
     class PropiedadController{
         public static function index(Router $router){
+            $propiedades = Propiedad::all();
+            $resultado = null;
             $router->render('propiedades/admin', [
-                'mensaje'=>'Desde la Vista'
+                'propiedades' => $propiedades,
+                'resultado' => $resultado
             ]);
         }
         public static function crear(){
