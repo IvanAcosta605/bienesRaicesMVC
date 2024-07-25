fa3bd172fc8c7579b6d5bda041da6220322e8e9c
IvanDev <ivanjoelacosta605@gmail.com>
2024-07-25 13:57:02 -0400

    Creando la página de Nosotros

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -1,13 +1,24 @@
 <?php
     namespace Controllers;
 
+    use MVC\Router;
+    use Model\Propiedad;
+
     class PaginasController{
-        public static function index(){
-            echo "Desde index";
+        public static function index(Router $router){
+
+            $inicio = true;
+
+            $propiedades = Propiedad::get(3);
+
+            $router->render('paginas/index', [
+                'inicio' => $inicio,
+                'propiedades' => $propiedades
+            ]);
         }
 
-        public static function nosotros(){
-            echo "Desde nosotros";
+        public static function nosotros(Router $router){
+            $router->render('paginas/nosotros', []);
         }
 
         public static function propiedades(){
---
+++ views/paginas/nosotros.php
@@ -0,0 +1,27 @@
+<main class="contenedor seccion">
+    <h1>Conoce sobre Nosotros</h1>
+    <div class="contenido-nosotros">
+        <div class="imagen">
+            <picture>
+                <source srcset="build/img/nosotros.webp" type="image/webp">
+                <source srcset="build/img/nosotros.jpg" type="image/jpeg">
+                <img src="build/img/nosotros.jpg" alt="sobre_nosotros" loading="lazy">
+            </picture>
+        </div>
+        <div class="texto-nosotros">
+            <blockquote>25 Años de experiencia</blockquote>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore tenetur autem eos non provident. Iusto nam adipisci laborum quisquam inventore nesciunt ipsum, modi eum totam veniam tenetur enim, nihil ullam!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto animi sit impedit necessitatibus consectetur nihil nemo commodi laborum nobis? Et excepturi recusandae aperiam molestiae, voluptatum a tempore enim minima esse?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, quibusdam ducimus est ipsum aliquam explicabo aspernatur quam amet similique! Atque minus consequatur vel officia enim natus recusandae harum voluptatum a?
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis alias dolorem accusamus eos quam. Tempore nemo aliquid perferendis ducimus animi, fugit nihil, quam debitis eligendi illum dicta voluptate ipsum alias.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat enim unde rerum molestiae maxime commodi, alias placeat, dolorem officiis obcaecati consectetur sint aliquid quidem voluptatibus vero non laudantium voluptas nesciunt?
+            </p>
+        </div>
+    </div>
+</main>
+<section class="contenedor seccion">
+    <?php
+        include 'iconos.php'; 
+    ?>
+</section>
