78b6d97e4e6b329ffe72fb5336804774251b220b
IvanDev <ivanjoelacosta605@gmail.com>
2024-07-26 15:08:54 -0400

    Ocultar o Mostrar los campos del formulario según la selección del usuario

--- src/js/app.js
+++ src/js/app.js
@@ -36,6 +36,25 @@ function navegacionResponsive(){
         navegacion.classList.add('mostrar');
     }
 }
-function mostrarMetodosContacto(){
-    console.log('seleccionando...')
+function mostrarMetodosContacto(e){
+    const contactoDiv = document.querySelector('#contacto');
+    if(e.target.value === 'telefono'){
+        contactoDiv.innerHTML = `
+            <label for="telefono">Teléfono</label>
+            <input type="tel" placeholder="Tu Teléfono" id="telefono" name="contacto[telefono]">
+
+            <p>Seleccione la fecha y la hora para la llamada</p>
+            <label for="fecha">Fecha:</label>
+            <input type="date" id="fecha" name="contacto[fecha]">
+            <label for="hora">Hora</label>
+            <input type="time" id="hora" min="09:00" max="18:00" name="contacto[hora]">
+        
+        `;
+    }else{
+        contactoDiv.innerHTML = `
+            <label for="email">E-mail</label>
+            <input type="email" placeholder="Tu E-mail" id="email" name="contacto[email]" required>
+        
+        `;
+    }
 }
--- views/paginas/contacto.php
+++ views/paginas/contacto.php
@@ -11,20 +11,16 @@
             <legend>Información Personal</legend>
             <label for="nombre">Nombre</label>
             <input type="text" placeholder="Tu Nombre" id="nombre" name="contacto[nombre]" required>
-            <label for="email">E-mail</label>
-            <input type="email" placeholder="Tu E-mail" id="email" name="contacto[email]" required>
-            <label for="telefono">Teléfono</label>
-            <input type="tel" placeholder="Tu Teléfono" id="telefono" name="contacto[telefono]" required>
             <label for="mensaje">Mensaje</label>
             <textarea id="mensaje" name="contacto[mensaje]"></textarea>
         </fieldset>
         <fieldset>
             <legend>Información sobre Propiedad</legend>
-            <label for="opciones">Vende o Compra</label>
+            <label for="opciones">Compra o Vende</label>
             <select id="opciones" name="contacto[tipo]" required>
                 <option value="" disabled selected>--Seleccionar--</option>
-                <option value="vende">Vende</option>
                 <option value="compra">Compra</option>
+                <option value="vende">Vende</option>
             </select>
             <label for="precio">Precio o Presupuesto</label>
             <input type="number" placeholder="Tu Precio o Presupuesto" id="precio" name="contacto[precio]" required>
@@ -33,16 +29,12 @@
             <legend>Contacto</legend>
             <p>Cómo desea ser contactado</p>
             <div class="forma-contacto">
-                <label for="contactar-telefono">Teléfono</label>
+                <label for="contactar-telefono">Llamada</label>
                 <input type="radio" value="telefono" id="contactar-telefono" name="contacto[contacto]" required>
                 <label for="contactar-email">E-mail</label>
                 <input type="radio" value="email" id="contactar-email" name="contacto[contacto]" required>
             </div>
-            <p>Si eligió teléfono, seleccione la fecha y la hora</p>
-            <label for="fecha">Fecha:</label>
-            <input type="date" id="fecha" name="contacto[fecha]">
-            <label for="hora">Hora</label>
-            <input type="time" id="hora" min="09:00" max="18:00" name="contacto[hora]">
+            <div id="contacto"></div>
         </fieldset>
         <input type="submit" value="Enviar" class="boton-verde">
     </form>
